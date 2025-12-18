fa502182fbe41a940b46f2a9271a7a5df2391c50
hibana2077 <hibana2077@gmail.com>
2025-12-19 09:30:29 +1000

    feat: add new training script IR006.sh for Super Mario World with specified parameters and intrinsic reward settings

---
+++ script/IR/IR006.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#PBS -P rp06
+#PBS -q gpuvolta
+#PBS -l ngpus=1            
+#PBS -l ncpus=12            
+#PBS -l mem=32GB           
+#PBS -l walltime=24:00:00  
+#PBS -l wd                  
+#PBS -l storage=scratch/rp06
+
+module load cuda/12.6.2
+module load ffmpeg/4.1.3
+
+source /scratch/rp06/sl5952/RL-test/.venv/bin/activate
+export HF_HOME="/scratch/rp06/sl5952/RL-test/.cache"
+export HF_HUB_OFFLINE=1
+
+cd ../..
+python3 -m retro.import ./roms
+python3 src/main.py \
+	--game "SuperMarioWorld-Snes" \
+	--state "YoshiIsland1" \
+	--total-steps 5000000 \
+	--train-chunk 25000 \
+	--n-envs 1 \
+	--n-steps 1024 \
+	--batch-size 128 \
+	--n-epochs 10 \
+	--learning-rate 5e-4 \
+	--gamma 0.995 \
+	--kl-coef 0.01 \
+	--ent-coef 0.01 \
+	--clip-range 0.3 \
+	--eval-episodes 2 \
+	--eval-max-steps 6000 \
+	--record-steps 3000 \
+    --reward-scale 0.05 \
+	--intrinsic-enable \
+	--intrinsic-scale 0.1 \
+	--intrinsic-w-curiosity 1.0 \
+	--intrinsic-w-novelty 1.0 \
+	--intrinsic-w-surprise 1.0 \
+	--log-dir "./runs_IR006" \
+	--device "cuda:0" \
+	>> IR006.log 2>&1
